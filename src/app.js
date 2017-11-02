07f490801b2f32b4b0097ad1cf0c3676d7d1f1bc
Stefan <stefan.burla@inomedica.ro>
2017-11-02 11:50:22 +0200

    setup express

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
-const http = require('http'); 
-const hostname = '192.168.0.112'; 
-const port = 3000; 
-const server = http.createServer();
-server.on('request', (request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello Worlds\n');
-});
+//import express
+const express = require('express');
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+//setup express
+const app = express();
+
+//listen for requests
+app.listen(process.env.port || 3000, () => {
+  console.log('listening for requests...');
+})
